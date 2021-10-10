73e685d6767013d6fb83a4732a5a036e30a59831
kolserdav <serega12101983@gmail.com>
2021-10-10 12:59:38 +0700

    Save example App.js

--- app/src/App.js
+++ app/src/App.js
@@ -4,11 +4,31 @@ import * as api from './api/api';
 
 function App(args) {
   useEffect(() => {
-    /**
-     * Учитывая что это не tsx файл то чтобы проверить типы
-     * нужно вызвать исходник userFindFirst в низу api/api.ts
-     */
-    api.userFindFirst();
+    (async () => {
+      console.log(1);
+      const createRes = await api.create({
+        data: {
+          name: 'User 8',
+          role: 'admin',
+          email: 'test8@test.test',
+        },
+      });
+      /**
+       * Учитывая что это не tsx файл то чтобы проверить типы
+       * нужно вызвать исходник userFindFirst в низу api/api.ts
+       */
+      const result = await api.userFindFirst({
+        where: {
+          role: 'admin',
+        },
+        select: {
+          name: true,
+          id: true,
+        },
+      });
+      console.log('userFindFirst-result', result);
+      console.log('createUser-result', createRes);
+    })();
   }, []);
   return (
     <div className="App">
