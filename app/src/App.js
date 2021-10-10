ba0c3c565258857b02372ac75b644def01e53b1f
kolserdav <serega12101983@gmail.com>
2021-10-10 08:41:12 +0700

    Start version

--- app/src/App.js
+++ app/src/App.js
@@ -1,45 +1,18 @@
-//@ts-check
-
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import * as utils from './utils';
 
-/*
-T extends P.UserFindFirstArgs>(
-  args: P.SelectSubset<T, P.UserFindFirstArgs>
-): Promise<
-  P.CheckSelect<
-    T,
-    Types.Api.Result<User | null>,
-    PrismaPromise<Types.Api.Result<P.UserGetPayload<T>>>
-  >
-
-*/
-
-/**
- *
- */
-function userFindFirst(args) {}
-/**
- * @
-
-  return
- */
 function App(args) {
+  useEffect(() => {
+    /**
+     * Учитывая что это не tsx файл то чтобы проверить типы
+     * нужно вызвать исходник userFindFirst в низу api/api.ts
+     */
+    utils.userFindFirst();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
